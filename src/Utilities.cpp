7394a70722c9b0c64d06f0cbbefb9df08748952d
Dylan Chapp <chapp1@llnl.gov>
2019-09-16 09:18:56 -0700

    Moved validate_dumpi_event specializations out of header to stop violating one-definition-rule

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -17,3 +17,147 @@ int trace_file_to_rank( const std::string trace_file )
     throw std::runtime_error( ss.str() );
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+template<>
+void validate_dumpi_event( dumpi_waitsome* event_ptr,
+                           const dumpi_time* cpu_time_ptr,
+                           const dumpi_time* wall_time_ptr,
+                           const dumpi_perfinfo* perf_counter_ptr )
+{
+  if ( event_ptr     == nullptr ||
+       cpu_time_ptr  == nullptr ||
+       wall_time_ptr == nullptr 
+     ) {
+    std::stringstream ss;
+    ss << "corrupted waitsome data - exiting" << std::endl;
+    throw std::runtime_error( ss.str() );
+  } 
+  else {
+    if ( event_ptr->requests == nullptr || event_ptr->indices  == nullptr ) {
+      std::stringstream ss;
+      ss << "waitsome requests/indices are null" << std::endl;
+      throw std::runtime_error( ss.str() );
+    }
+  }
+}
+
+template<>
+void validate_dumpi_event( dumpi_testsome* event_ptr,
+                           const dumpi_time* cpu_time_ptr,
+                           const dumpi_time* wall_time_ptr,
+                           const dumpi_perfinfo* perf_counter_ptr )
+{
+  if ( event_ptr     == nullptr ||
+       cpu_time_ptr  == nullptr ||
+       wall_time_ptr == nullptr 
+     ) {
+    std::stringstream ss;
+    ss << "corrupted testsome data - exiting" << std::endl;
+    throw std::runtime_error( ss.str() );
+  } 
+  else {
+    if ( event_ptr->requests == nullptr || event_ptr->indices  == nullptr ) {
+      std::stringstream ss;
+      ss << "testsome requests/indices are null" << std::endl;
+      throw std::runtime_error( ss.str() );
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+template<>
+void validate_dumpi_event( dumpi_waitall* event_ptr,
+                           const dumpi_time* cpu_time_ptr,
+                           const dumpi_time* wall_time_ptr,
+                           const dumpi_perfinfo* perf_counter_ptr )
+{
+  if ( event_ptr     == nullptr ||
+       cpu_time_ptr  == nullptr ||
+       wall_time_ptr == nullptr 
+     ) {
+    std::stringstream ss;
+    ss << "corrupted waitall data - exiting" << std::endl;
+    throw std::runtime_error( ss.str() );
+  } 
+  else {
+    if ( event_ptr->requests == nullptr ) {
+      std::stringstream ss;
+      ss << "waitall requests are null" << std::endl;
+      throw std::runtime_error( ss.str() );
+    }
+  }
+}
+
+template<>
+void validate_dumpi_event( dumpi_waitany* event_ptr,
+                           const dumpi_time* cpu_time_ptr,
+                           const dumpi_time* wall_time_ptr,
+                           const dumpi_perfinfo* perf_counter_ptr )
+{
+  if ( event_ptr     == nullptr ||
+       cpu_time_ptr  == nullptr ||
+       wall_time_ptr == nullptr 
+     ) {
+    std::stringstream ss;
+    ss << "corrupted waitany data - exiting" << std::endl;
+    throw std::runtime_error( ss.str() );
+  } 
+  else {
+    if ( event_ptr->requests == nullptr ) {
+      std::stringstream ss;
+      ss << "waitany requests are null" << std::endl;
+      throw std::runtime_error( ss.str() );
+    }
+  }
+}
+
+template<>
+void validate_dumpi_event( dumpi_testall* event_ptr,
+                           const dumpi_time* cpu_time_ptr,
+                           const dumpi_time* wall_time_ptr,
+                           const dumpi_perfinfo* perf_counter_ptr )
+{
+  if ( event_ptr     == nullptr ||
+       cpu_time_ptr  == nullptr ||
+       wall_time_ptr == nullptr 
+     ) {
+    std::stringstream ss;
+    ss << "corrupted testall data - exiting" << std::endl;
+    throw std::runtime_error( ss.str() );
+  } 
+  else {
+    if ( event_ptr->requests == nullptr ) {
+      std::stringstream ss;
+      ss << "testall requests are null" << std::endl;
+      throw std::runtime_error( ss.str() );
+    }
+  }
+}
+
+template<>
+void validate_dumpi_event( dumpi_testany* event_ptr,
+                           const dumpi_time* cpu_time_ptr,
+                           const dumpi_time* wall_time_ptr,
+                           const dumpi_perfinfo* perf_counter_ptr )
+{
+  if ( event_ptr     == nullptr ||
+       cpu_time_ptr  == nullptr ||
+       wall_time_ptr == nullptr 
+     ) {
+    std::stringstream ss;
+    ss << "corrupted testany data - exiting" << std::endl;
+    throw std::runtime_error( ss.str() );
+  } 
+  else {
+    if ( event_ptr->requests == nullptr ) {
+      std::stringstream ss;
+      ss << "testany requests are null" << std::endl;
+      throw std::runtime_error( ss.str() );
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
