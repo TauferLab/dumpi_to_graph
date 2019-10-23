68114ee359daced78fe3e1a921ba16743be894fc
Dylan Chapp <chapp1@llnl.gov>
2019-10-22 19:27:00 -0700

    It appears that the bug in dumpi_to_graph causing it to deadlock when processing traces from miniAMR is related to miniAMR's use of user-defined communicators in its load-balancing routines. (Specifically, some of the p2p communication in rcb.c)
    With this in mind, dumpi_to_graph needs to do remote message matching
    exchange in a way that respects user-defined communicators. To do this,
    we need to track user-defined communicators as the traces are parsed.
    
    This commit adds stub callbacks to do this, at least to the degree
    necessary for miniAMR. We add callbacks for:
    - MPI_Comm_split
    - MPI_Comm_rank
    - MPI_Comm_size

---
+++ include/dumpi_to_graph/communicator_management_callbacks.hpp
@@ -0,0 +1,28 @@
+#ifndef D2G_COMMUNICATOR_MANAGEMENT_CALLBACKS_H
+#define D2G_COMMUNICATOR_MANAGEMENT_CALLBACKS_H
+
+// DUMPI-specific headers
+#include "dumpi/common/argtypes.h"
+
+int cb_MPI_Comm_split(const dumpi_comm_split *prm, 
+                      uint16_t thread, 
+                      const dumpi_time *cpu, 
+                      const dumpi_time *wall, 
+                      const dumpi_perfinfo *perf, 
+                      void *uarg);
+
+int cb_MPI_Comm_rank(const dumpi_comm_rank *prm, 
+                     uint16_t thread, 
+                     const dumpi_time *cpu, 
+                     const dumpi_time *wall, 
+                     const dumpi_perfinfo *perf, 
+                     void *uarg);
+
+int report_MPI_Comm_size(const dumpi_comm_size *prm, 
+                         uint16_t thread, 
+                         const dumpi_time *cpu, 
+                         const dumpi_time *wall, 
+                         const dumpi_perfinfo *perf, 
+                         void *uarg);
+
+#endif // D2G_COMMUNICATOR_MANAGEMENT_CALLBACKS_H
---
+++ src/communicator_management_callbacks.cpp
@@ -0,0 +1,34 @@
+#include "communicator_management_callbacks.hpp"
+
+// DUMPI-specific headers
+#include "dumpi/common/argtypes.h"
+
+int cb_MPI_Comm_split(const dumpi_comm_split *prm, 
+                      uint16_t thread, 
+                      const dumpi_time *cpu, 
+                      const dumpi_time *wall, 
+                      const dumpi_perfinfo *perf, 
+                      void *uarg) 
+{
+  return 0;
+}
+
+int cb_MPI_Comm_rank(const dumpi_comm_rank *prm, 
+                     uint16_t thread, 
+                     const dumpi_time *cpu, 
+                     const dumpi_time *wall, 
+                     const dumpi_perfinfo *perf, 
+                     void *uarg) 
+{
+  return 0;
+}
+
+int report_MPI_Comm_size(const dumpi_comm_size *prm, 
+                         uint16_t thread, 
+                         const dumpi_time *cpu, 
+                         const dumpi_time *wall, 
+                         const dumpi_perfinfo *perf, 
+                         void *uarg) 
+{
+  return 0;
+}
