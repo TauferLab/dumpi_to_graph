5484b9a38dfc312fe27df440648d4af756877e2d
Dylan Chapp <dchapp@udel.edu>
2020-02-26 12:47:05 -0800

    Updated build script to build in system-specific build directories

--- one_step_build.sh
+++ one_step_build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+system=$( hostname | sed 's/[0-9]*//g' )
+build_dir=build_${system}
+
 export CC=mpicc
 export CXX=mpicxx
-rm -rf $(pwd)/build && mkdir -p $(pwd)/build && cd build
-cmake -DDUMPI_ROOT=$HOME/ANACIN-X/submodules/sst-dumpi/build ..
+rm -rf $(pwd)/${build_dir} && mkdir -p $(pwd)/${build_dir} && cd ${build_dir}
+cmake -DDUMPI_ROOT=$HOME/ANACIN-X/submodules/sst-dumpi/${build_dir} ..
 make -j
